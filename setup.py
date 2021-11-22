35f4696dc2504a559c53c0a26d5552f354c305b5
Fernando Jerez <fernandojerez@gmail.com>
2021-11-21 22:46:28 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,11 @@ from setuptools import find_packages, setup
 
 setup(
     name='webrobot',
-    package=find_packages(include=['webrobot']),
+    packages=find_packages(include=['webrobot']),
     version='0.1',
     description='Api to control chromium browsers using puppeteer',
     author='@fernandojerez',
     license='Apache',
-    install_requires=['websockets', 'pydantic', 'beautifulsoup4'],
-    setup_requires=[],
-    test_requires=[],
+    requires=['websockets', 'pydantic', 'beautifulsoup4'],
     test_suite='tests'
 )
